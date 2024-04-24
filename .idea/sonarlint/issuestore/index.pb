866acf1891d535557cc002465093f25ce40f38d0
caijinyang <caijinyang98@gmail.com>
2024-04-24 15:44:25 +0800

    Chapter1: Kafka Fast Producer And Consumer

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+kafka/pom.xml,5\3\5392e9e64dad160ea46fe0b8e8a6835e658c3894
+k
+;kafka/src/main/java/com/example/kafka/KafkaApplication.java,f\b\fb8ab4fa3ce9747e573f59b26104729decabb510
+~
+Nkafka/src/main/java/com/example/kafka/produceAndConsume/ConsumerFastStart.java,f\b\fb6d3fe294f897bb95f8b56b799a7965d527be10
+~
+Nkafka/src/main/java/com/example/kafka/produceAndConsume/ProducerFastStart.java,f\f\ff10789e245f4e718b53803877caf7b1c4c0392a
+w
+G.idea/sonarlint/issuestore/1/d/1db6cbdf276ec7d69ec37a5a7eb829a504ae9d91,6\2\62cfb88d5e9ceedfcbf9a4dc234582f2dea1ca48
---
+++ kafka/src/main/java/com/example/kafka/produceAndConsume/ProducerFastStart.java
@@ -0,0 +1,37 @@
+package com.example.kafka.produceAndConsume;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author caijinyang
+ * @classname ProducerFastStart
+ * @description
+ * @date 2024/4/24
+ **/
+
+public class ProducerFastStart {
+    public static final String BROKER_LIST = "localhost:9092";
+    public static final String TOPIC = "topic-demo";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("bootstrap.servers", BROKER_LIST);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, "hello, Kafka!");
+        try {
+            // producer.send(record);
+            producer.send(producerRecord).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        producer.close();
+    }
+}
